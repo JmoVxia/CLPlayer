d379acba72cc7806193fe2ae7be7f496aab32d13
JmoVxia <269968846@qq.com>
2017-08-22 17:17:24 +0800

    修复越界引起崩溃bug

--- CLPlayer/UITabBarController+CLPlayerRotation.m
+++ CLPlayer/UITabBarController+CLPlayerRotation.m
@@ -7,8 +7,33 @@
 //
 
 #import "UITabBarController+CLPlayerRotation.h"
-
+#import <objc/runtime.h>
 @implementation UITabBarController (CLPlayerRotation)
++ (void)load {
+    SEL selectors[] = {
+        @selector(selectedIndex)
+    };
+    for (NSUInteger index = 0; index < sizeof(selectors) / sizeof(SEL); ++index) {
+        SEL originalSelector = selectors[index];
+        SEL swizzledSelector = NSSelectorFromString([@"cl_" stringByAppendingString:NSStringFromSelector(originalSelector)]);
+        Method originalMethod = class_getInstanceMethod(self, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(self, swizzledSelector);
+        if (class_addMethod(self, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))) {
+            class_replaceMethod(self, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    }
+}
+- (NSInteger)cl_selectedIndex {
+    NSInteger index = [self cl_selectedIndex];
+    if (index > self.viewControllers.count){
+        return 0;
+    }else{
+        return index;
+    }
+}
+
 // 是否支持自动转屏
 - (BOOL)shouldAutorotate {
     UIViewController *vc = self.viewControllers[self.selectedIndex];
